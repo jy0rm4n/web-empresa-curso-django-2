29168fd0002c0e86d7fb04dcee5172f04764f94e
jyorman <jy0rm4n@gmail.com>
2018-11-28 20:10:16 -0500

    Add project

---
+++ webempresa/blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Category
+# Create your views here.
+
+
+def blog(request):
+    posts = Post.objects.all()
+    return render(request, "blog/blog.htm", {'posts': posts})
+
+
+def category(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    return render(request, "blog/category.htm", {'category': category})
---
+++ webempresa/pages/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Page
+
+# Create your views here.
+
+
+def page(request, page_id, page_slug):
+    # Recuperamos la página
+    page = get_object_or_404(Page, id=page_id)
+    return render(request, 'pages/sample.htm', {'page': page})
